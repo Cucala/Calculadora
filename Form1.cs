1e67e841e9445fbdc56f27b14f131b48399d4c4a
Arturo Herrera Gismero <arturo_h_gismero@hotmial.es>
2022-05-24 17:36:30 +0200

    añadido el codigo de los numeros y del botón más

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,62 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+        int numero1;
+        int numero2;
+        int resultado;
+        private void oneButton_Click(object sender, EventArgs e)
+        {
+            txtPantalla.Text = txtPantalla.Text + "1";
+        }
+
+        private void plusButton_Click(object sender, EventArgs e)
+        {
+            txtPantalla.Text = txtPantalla.Text + "+";
+        }
+
+        private void fiveButton_Click(object sender, EventArgs e)
+        {
+            txtPantalla.Text = txtPantalla.Text + "5";
+        }
+
+        private void twoButton_Click(object sender, EventArgs e)
+        {
+            txtPantalla.Text = txtPantalla.Text + "2";
+        }
+
+        private void threeButton_Click(object sender, EventArgs e)
+        {
+            txtPantalla.Text = txtPantalla.Text + "3";
+        }
+
+        private void fourButton_Click(object sender, EventArgs e)
+        {
+            txtPantalla.Text = txtPantalla.Text + "4";
+        }
+
+        private void sixButton_Click(object sender, EventArgs e)
+        {
+            txtPantalla.Text = txtPantalla.Text + "6";
+        }
+
+        private void sevenButton_Click(object sender, EventArgs e)
+        {
+            txtPantalla.Text = txtPantalla.Text + "7";
+        }
+
+        private void eightButton_Click(object sender, EventArgs e)
+        {
+            txtPantalla.Text = txtPantalla.Text + "8";
+        }
+
+        private void nineButton_Click(object sender, EventArgs e)
+        {
+            txtPantalla.Text = txtPantalla.Text + "9";
+        }
+
+        private void zeroButton_Click(object sender, EventArgs e)
+        {
+            txtPantalla.Text = txtPantalla.Text + "0";
+        }
     }
 }
